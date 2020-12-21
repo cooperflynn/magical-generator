74ba38588fee4719d73cb23ee97bd0f72e18d275
Cooper <cooperflynn@utexas.edu>
2020-12-20 23:51:22 -0600

    wrapped in function

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
 
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -43,12 +50,6 @@ for(i=0; i < Number(lengthPrompt); i++) {
 }
 
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -58,3 +59,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
